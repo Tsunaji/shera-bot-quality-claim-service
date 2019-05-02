938d66120bdbe2d1846731f76a009d1ec86ae8de
Jirasak Kammakorn <jirasak.ka.92@gmail.com>
2019-05-02 17:07:19 +0700

    fix MS Teams image contentUrl issue 2

--- bot.js
+++ bot.js
@@ -792,7 +792,7 @@ class MyBot {
                     if (step.context.activity.channelId === 'skype' || step.context.activity.channelId === 'msteams') {
                         
                         // prepare contentUrl
-                        const tempContentUrl = user.images[i].contentUrl.substr(0, t.lastIndexOf("views") + 1) + '\origunal';
+                        const tempContentUrl = user.images[i].contentUrl.substr(0, user.images[i].contentUrl.lastIndexOf("views") + 1) + '\origunal';
                         user.images[i].contentUrl = tempContentUrl;
 
                         const contentUrl = user.images[i].contentUrl;
