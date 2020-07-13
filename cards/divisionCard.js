ec73af705a01de7557c8cf021bf8082b5d9f809d
Jirasak Kammakorn <jirasak_ka@shera.com>
2020-07-13 16:05:53 +0700

    fix sales area text, clean project

--- cards/divisionCard.js
+++
@@ -1,8 +0,0 @@
-const { CardFactory, ActionTypes } = require('botbuilder');
-const ProductList = require('../resources/productEN.json');
-const path = require('path');
-const fs = require('fs');
-
-module.exports = {
-    
-}
